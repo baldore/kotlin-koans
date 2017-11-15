33c3f2e3093f5178fd27c76e1725b4036a1c4f7b
baldore <acornssn@gmail.com>
2017-11-15 14:41:48 -0500

    Completes ii_23 - CompoundTasks

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -1,18 +1,37 @@
 package ii_collections
 
+// Return the set of customers who ordered the specified product
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter {
+        it.orders
+            .flatMap { it.products }
+            .toSet()
+            .contains(product)
+    }.toSet()
 }
 
+// Return the most expensive product among all delivered products
+// (use the Order.isDelivered flag)
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
-    // Return the most expensive product among all delivered products
-    // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    val deliveredProducts = orders
+        .filter { it.isDelivered }
+        .flatMap { it.products }
+        .toSet()
+
+    if (deliveredProducts.isEmpty()) {
+        return null
+    }
+
+    return deliveredProducts.reduce({ lastProduct, nextProduct ->
+        if (lastProduct.price > nextProduct.price) lastProduct else nextProduct
+    })
 }
 
+// Return the number of times the given product was ordered.
+// Note: a customer may order the same product for several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    // Return the number of times the given product was ordered.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    val allProducts = customers
+        .flatMap { it.orders }
+        .flatMap { it.products }
+    return allProducts.count { it == product }
 }
